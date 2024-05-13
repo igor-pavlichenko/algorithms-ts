02d5540af7918626627f02554447ca1338817020
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-05-13 22:57:56 +0100

    Implement binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0; // start is inclusive
+    let high = haystack.length; // end is exclusive
 
+    do {
+        const mid = Math.floor(low + (high - low) / 2); // need to know this formula
+        const value = haystack[mid];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            // then throw out everything to the right
+            high = mid;
+        } else if (value < needle) {
+            // then throw away everything to the left
+            low = mid + 1; // no need to check mid again
+        }
+    } while (low < high);
+
+    return false;
 }
