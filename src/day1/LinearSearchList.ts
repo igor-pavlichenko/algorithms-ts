5b88cd538b6d8b0c5224a3838f29ed7e64354399
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-05-13 14:19:35 +0100

    Implement linear search

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,1 +1,7 @@
-export default function linear_search(haystack: number[], needle: number): boolean {}
+export default function linear_search(haystack: number[], needle: number): boolean {
+    for (let i = 0; i < haystack.length; i++) {
+        const element = haystack[i];
+        if (element === needle) return true;
+    }
+    return false;
+}
