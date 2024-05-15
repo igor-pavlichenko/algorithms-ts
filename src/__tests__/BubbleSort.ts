9a8f2b8a05d390204d60c3dfe653ad315f67141c
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-05-15 23:13:54 +0100

    Implement bubble sort

--- src/__tests__/BubbleSort.ts
+++ src/__tests__/BubbleSort.ts
@@ -1,6 +1,6 @@
-import bubble_sort from "@code/BubbleSort";
+import bubble_sort from '@code/BubbleSort';
 
-test("bubble-sort", function () {
+test('bubble-sort', function () {
     const arr = [9, 3, 7, 4, 69, 420, 42];
 
     debugger;
@@ -8,3 +8,10 @@ test("bubble-sort", function () {
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
 });
 
+test('bubble sort bigger array', () => {
+    const unsortedArray = [453, 45, 9, 3423, 6, 1, 45345, 45454, 1212, 1211, 1004, 5, 8, 0];
+    const sortedArray = [0, 1, 5, 6, 8, 9, 45, 453, 1004, 1211, 1212, 3423, 45345, 45454];
+    bubble_sort(unsortedArray);
+
+    expect(unsortedArray).toEqual(sortedArray);
+});
--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,24 @@
+/**
+ * Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm
+ * that repeatedly steps through the input list element by element, comparing
+ * the current element with the one after it, swapping their values if needed.
+ *
+ * Worst-case complexity: O(n^2)
+ * Best complexity: O(n)
+ * Average complexity: O(n^2)
+ */
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        // remember to subtract "- 1 - i" because every iteration
+        // the biggest number gets sorted to the end, so no need to keep comparing them
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            const current = arr[j];
+            const next = arr[j + 1];
+            if (current > next) {
+                // swap them
+                arr[j + 1] = current;
+                arr[j] = next;
+            }
+        }
+    }
 }
