97635697310c2975d36762cfefab7e154d425503
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-06-03 23:27:24 +0100

    Add some rationale comments about recursion

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,26 @@
+/**
+ * example maze:
+ *
+ *
+ *
+ *            ⬇ end here
+ *      "#####  #"
+ *      "#      #"
+ *      "#  #####"
+ *        ⬆ start here
+ *
+ *
+ * use recursion... define base cases:
+ * 1. off the map
+ * 2. it's a wall
+ * 3. it's the end
+ * 4. if we have seen it
+ *
+ * remember that the entrance to the recursive function is often not the one we are going to recurse in
+ *
+ * @returns list of points from the start to the end
+ */
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
-
+    return [];
 }
+
