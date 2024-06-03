67259b3024a7994069c354b4f1494772a3e8eec3
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-06-03 23:43:14 +0100

    Implement the base case of recursion

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -24,3 +24,34 @@ export default function solve(maze: string[], wall: string, start: Point, end: P
     return [];
 }
 
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]) {
+    // 1. Base case
+    // off the map
+    if (
+        // columns check
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        // rows check
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
+
+    // it's a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // it's the end
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
+    }
+
+    // we have seen it
+    // we could mutate the point to add a flag but that would be weird
+    // instead, we'll pass around a 2D array with all coordinates we visited, true if we've seen it, false if we didn't
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+}
