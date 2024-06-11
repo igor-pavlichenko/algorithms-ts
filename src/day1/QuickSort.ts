4e6720b20f69d7b5e3e31f7aac0fcee807547b9f
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-06-11 03:19:28 +0100

    Write the partition part of the algorithm

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -24,13 +24,33 @@ export default function quick_sort(arr: number[]): void {
  * 
  * @param arr 
  * @param lo abbreviation of LOW
- * @param hi abbreviation of HIGH
+ * @param hi abbreviation of HIGH, unusual thing - both LOW & HIGH are inclusive
  */
 function qs(arr: number[], lo: number, hi: number) {
 	
 }
 
 function partition(arr: number[], lo: number, hi: number): number {
-	
-	return 0;
+	const pivot = arr[hi]; // there are many variations for to pivot, this is the simplest
+	let idx = lo - 1; // will be incremented whenever we find an element >= pivot
+
+	// now we need to walk from "lo" to "hi" but not including "hi" because it's the pivot
+	// and do weak sort on this sub-array
+	for (let i = lo; i < hi; i++) {
+		if (arr[i] <= pivot) {
+			idx++;
+			const tmp = arr[i];
+			arr[i] = arr[idx];
+			arr[idx] = tmp;
+		}
+	}
+	// by now we moved everything thet is <= pivot to beginning
+	// now we need to move our pivot to idx position
+	// so that we hold true to the rule of
+	// "everything to the left of pivot needs to be <= pivot and everything right needs to be > pivot"
+	idx++;
+	arr[hi] = arr[idx]; // move this element to the end where pivot was
+	arr[idx] = pivot; // move pivot to it's final position
+
+	return idx; // return new index of pivot
 }
