8c9501b428b1a06a0c9740734d17fc3fcf8263f4
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-06-14 00:57:35 +0100

    Implement prepend

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,44 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+};
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
+    // complexity: O(1)
     prepend(item: T): void {
+        const node: Node<T> = { value: item };
+        this.length++;
 
-}
-    insertAt(item: T, idx: number): void {
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
 
-}
-    append(item: T): void {
+    insertAt(item: T, idx: number): void {}
 
-}
+    append(item: T): void {}
+
     remove(item: T): T | undefined {
+        return undefined;
+    }
 
-}
     get(idx: number): T | undefined {
+        return undefined;
+    }
 
-}
     removeAt(idx: number): T | undefined {
-
-}
+        return undefined;
+    }
 }
