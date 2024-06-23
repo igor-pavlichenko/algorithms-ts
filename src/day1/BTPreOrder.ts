9ed85f588019dcf05e142d70ea2863c10741ec1b
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-06-23 02:27:24 +0100

    Implement binary tree search with pre-order

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,29 @@
+/**
+ * we are given the head/root of the tree, and we need to visit all the nodes and
+ * return them in "pre order"
+ */
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
+}
 
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // base case is when our node is non-existent
+    if (!curr) {
+        return path;
+    }
+
+    // recursion steps:
+    // 1 - pre
+    // 2 - recurse
+    // 3 - post
+
+    // pre
+    path.push(curr.value);
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post
+    return path;
 }
