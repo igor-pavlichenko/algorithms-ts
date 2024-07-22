b65b9e947473fb1b1c6daf925fa5c8b6a7c55bcf
Igor Pavlichenko <igor.o.pavlichenko@gmail.com>
2024-07-22 20:33:37 +0100

    Implement binary tree comparison algorithm

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,14 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // base cases
+    if (a === null && b === null) return true;
+    if (a === null || b === null) return false;
+    if (a.value !== b.value) return false;
 
+    // it will keep checking base cases on left subtree & right subtree and
+    // if it reaches both null ends returning true then they are equal
+    return compare(a.left, b.left) && compare(a.right, b.right);
+    // it's depth first because it will go all the way left first
 }
